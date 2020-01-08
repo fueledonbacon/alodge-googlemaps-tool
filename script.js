6c8f3b5ab7a81789959594e2ba9b1551ecce3cba
Ryan Cwynar <ryan@fueledonbacon.com>
2020-01-07 19:56:56 -0500

    add a descripton block below the map

--- script.js
+++ script.js
@@ -17,7 +17,14 @@ class MapLocation{
     return inputArr.map((input) => new MapLocation(input))
   }
 
-  constructor({ name, address, lat, long, description = '', icon = '' }){
+  constructor({
+    name,
+    address,
+    lat,
+    long,
+    description = '',
+    icon = ''
+  }){
 
     this.icon = icon
     this.name = name
@@ -28,6 +35,13 @@ class MapLocation{
   }
 
   get infoHTML(){
+
+    return `<strong>${this.name}</strong><br>
+    ${this.address}<br>
+    <a href="${this.link.href}">Get Directions</a>`
+  }
+
+  get descriptionHTML(){
 
     return `<strong>${this.name}</strong><br>
     ${this.address}<br>
@@ -53,39 +67,13 @@ class GMapsLink{
 
 class GMap{
 
-  static example(){
-
-    const map = new GMap({
-      id: 'map',
-      lat: 41.976816,
-      long: -87.659916
-    })
-
-    const locations = MapLocation.initMany([
-      {
-        name: 'Chipotle on Broadway',
-        address: '5224 N Broadway St<br> Chicago, IL 60640',
-        lat: 41.976816,
-        long: -87.659916
-      },
-      {
-        name: 'Chipotle on Belmont',
-        address: '1025 W Belmont Ave<br> Chicago, IL 60657',
-        lat: 41.939670,
-        long: -87.655167
-      },
-      {
-        name: 'Chipotle on Sheridan',
-        address: '6600 N Sheridan Rd<br> Chicago, IL 60626',
-        lat: 42.002707,
-        long: -87.661236
-      }
-    ])
-  
-    map.addMarkers(locations)
-  }
-
-  constructor({ id, lat, long, mapType = 'ROADMAP', zoom = 13 }){
+  constructor({ id,
+    descriptionBlockId = 'location-description',
+    lat, 
+    long, 
+    mapType = 'ROADMAP', 
+    zoom = 12
+  }){
 
     this.instance = new google.maps.Map(document.getElementById(id), {
       zoom,
@@ -94,7 +82,7 @@ class GMap{
     })
 
     this.infoWindow = new google.maps.InfoWindow({})
-    
+    this.descriptionBlock = document.getElementById(descriptionBlockId)
   }
 
   addMarkers(locations){
@@ -113,6 +101,7 @@ class GMap{
     })
 
     this.addInfoWindowListener(location, marker)
+    this.addLocationDescriptionListener(location, marker)
   }
 
   addInfoWindowListener(location, marker){
@@ -122,8 +111,11 @@ class GMap{
       this.infoWindow.open(this.instance, marker);
     });
   }
-}
 
-function initMap() {
-  GMap.example()
+  addLocationDescriptionListener(location, marker){
+
+    google.maps.event.addListener(marker, 'click', () => {
+      this.descriptionBlock.innerHTML = location.descriptionHTML
+    });
+  }
 }
