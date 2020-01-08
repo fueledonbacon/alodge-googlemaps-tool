1190332879d72a834c2fd33286f9b1633ed3ba3e
Ryan Cwynar <ryan@fueledonbacon.com>
2020-01-07 19:10:51 -0500

    pass lat long into constructor, not objects'

--- script.js
+++ script.js
@@ -34,11 +34,11 @@ class MapLocation{
     return inputArr.map((input) => new MapLocation(input))
   }
 
-  constructor({ name, address, coordinate, description = '', icon = '' }){
+  constructor({ name, address, lat, long, description = '', icon = '' }){
     this.icon = icon
     this.name = name
     this.address = address
-    this.coordinate = coordinate
+    this.coordinate = new LatLng(lat, long),
     this.description = description
     this.link = new GMapsLink(this.coordinate)
   }
@@ -54,34 +54,41 @@ class MapLocation{
 class GMap{
 
   static example(){
-    const map = new GMap('map', new LatLng(41.976816, -87.659916))
+    const map = new GMap({
+      id: 'map',
+      lat: 41.976816,
+      long: -87.659916
+    })
 
     const locations = MapLocation.initMany([
       {
         name: 'Chipotle on Broadway',
         address: '5224 N Broadway St<br> Chicago, IL 60640',
-        coordinate: new LatLng(41.976816, -87.659916)
+        lat: 41.976816,
+        long: -87.659916
       },
       {
         name: 'Chipotle on Belmont',
         address: '1025 W Belmont Ave<br> Chicago, IL 60657',
-        coordinate: new LatLng(41.939670, -87.655167)
+        lat: 41.939670,
+        long: -87.655167
       },
       {
         name: 'Chipotle on Sheridan',
         address: '6600 N Sheridan Rd<br> Chicago, IL 60626',
-        coordinate: new LatLng(42.002707, -87.661236)
+        lat: 42.002707,
+        long: -87.661236
       }
     ])
   
     map.addMarkers(locations)
   }
 
-  constructor( domId, center, mapType = 'ROADMAP', zoom = 13){
+  constructor({ id, lat, long, mapType = 'ROADMAP', zoom = 13 }){
 
-    this.instance = new google.maps.Map(document.getElementById(domId), {
+    this.instance = new google.maps.Map(document.getElementById(id), {
       zoom,
-      center: new google.maps.LatLng(center.lat, center.long),
+      center: new google.maps.LatLng(lat, long),
       mapTypeId: google.maps.MapTypeId[mapType]
     })
 
